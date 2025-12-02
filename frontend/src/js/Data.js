fdccde08c97f6d8d95a3726c6724c13ee15c2a5c
MrVishalKuriya <vishalpravinbhai6@gmail.com>
2025-12-02 21:23:36 +0530

    deleted all

--- frontend/src/js/Data.js
+++
@@ -1,6 +0,0 @@
-
-export const measurementUnits = ["px", "em", "rem", "%", "vh", "vw"];
-export const colors = ["red", "blue", "green", "yellow", "pink", "orange", "grey", "purple", "brown", "black", "white"];
-export const fontFamilies = ["Arial", "Helvetica", "Times New Roman", "Courier New", "Verdana", "Georgia", "Palatino", "Garamond", "Comic Sans MS", "Trebuchet MS"];
-export const fontSizes = ["12px", "14px", "16px", "18px", "20px", "24px", "28px", "32px", "36px", "40px"];
-export const spacingValues = ["0px", "5px", "10px", "15px", "20px", "25px", "30px", "35px", "40px", "45px", "50px"];
